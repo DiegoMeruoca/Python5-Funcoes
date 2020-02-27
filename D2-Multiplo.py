b5f3def59daa2d5bbf4139eb2670be9214af6409
DiegoMeruoca <diegomeruoca@gmail.com>
2020-02-26 23:30:48 -0300

    Atualizado 1º semestre 2020

---
+++ D2-Multiplo.py
@@ -0,0 +1,12 @@
+def multiplo(n1, n2):
+    if n1 % n2 == 0:
+        return "é multiplo"
+    else:
+        return "não é multiplo"
+
+
+# Programa principal
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+print("O primeiro número {} do segundo!" .format(multiplo(num1, num2)))
+
---
+++ Ex1-Soma_v1.py
@@ -0,0 +1,7 @@
+def soma(a, b):  # Definição da função e seus parâmetros
+    return a + b  # Valor de retorno
+
+
+# Programa principal
+print(soma(2, 9))
+print(soma(11, 19))
